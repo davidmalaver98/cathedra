6c49300ac5efd5477497bda5c3507e5ade06bace
davidmalaver98 <damalavarr@academia.usbbog.edu.co>
2026-04-21 19:57:56 +0000

    .

--- cathedra_back/cathedra_/cathedra/settings.py
+++ cathedra_back/cathedra_/cathedra/settings.py
@@ -75,15 +75,10 @@ WSGI_APPLICATION = 'cathedra.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'cathedra',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
