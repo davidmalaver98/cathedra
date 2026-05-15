5d4c46e38b1f8029766d92c02b423018ed68cd9b
davidmalaver98 <damalavarr@academia.usbbog.edu.co>
2026-05-15 16:45:48 +0000

    login corregido y rediseñado

--- cathedra_back/cathedra_/usuarios/views.py
+++ cathedra_back/cathedra_/usuarios/views.py
@@ -1,5 +1,8 @@
+
+
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 
 def login_view(request):
@@ -22,3 +25,26 @@ def logout_view(request):
 @login_required
 def inicio_view(request):
     return render(request, 'inicio.html')
+
+def registro_view(request):
+    error = None
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        first_name = request.POST['first_name']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        if password1 != password2:
+            error = 'Las contraseñas no coinciden'
+        elif User.objects.filter(username=username).exists():
+            error = 'El usuario ya existe'
+        else:
+            User.objects.create_user(
+                username=username,
+                email=email,
+                first_name=first_name,
+                password=password1
+            )
+            return redirect('login')
+    return render(request, 'login.html', {'error': error})
